b0eaa0e11c2b0d8c5acd2b95aadca9f52a6af834
Chris Junker <dancetherapeutics@gmail.com>
2017-11-17 20:36:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,5 +16,5 @@ function changeLeastFavoriteCustomer(){
 }
 let favoriteCustomer = 'John Taylor'
 function attemptTwoFavoriteCustomers(){
-   let favoriteCustomer = 'Brendon Jones';
+   let favoriteCustomer = 'John Taylor';
 }
