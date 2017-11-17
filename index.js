53edb1de5abe4e4b4cfe75fcd46fafabfb55f496
Chris Junker <dancetherapeutics@gmail.com>
2017-11-17 20:15:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // Write your solution in this file!
 var customerName = 'bob'
+function upperCaseCustomerName(customerName){
+  return customerName;
+}
+
 function setBestCustomer(){
    bestCustomer = 'not bob';
 }
